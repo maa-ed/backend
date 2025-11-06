394ca77f1a93c3a076e4971409413580c78bbd4a
Firomsa Assefa <firomsassf@gmail.com>
2025-11-06 04:54:29 +0300

    finished initial setup

---
+++ src/main/java/com/firomsa/maaedBackend/MaaedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.firomsa.maaedBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaaedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaaedBackendApplication.class, args);
+	}
+
+}
